6d56bff75ebf751fea61fa2ee21dee0aa620079e
kedmonjoseph <jkedmon95@gmail.com>
2020-10-02 17:03:52 +0300

    reduce pandings

--- resources/views/love/create.blade.php
+++ resources/views/love/create.blade.php
@@ -82,21 +82,21 @@
     <div class="container">
             <div class="row">
                 <div class="col-sm-12 text-center bg">
-                    <h2 class="pb-5 pt-5 text-white">Real Love Calculator</h2>
-                    <h5 class="pb-5 text-white">Find Secret Lover of your Friends using this Fake Love Calculator.</h5>
+                    <h2 class="pb-3 pt-3 text-white">Love Calculator</h2>
+                    <h5 class="pb-3 text-white">Find Secret Lover of your Friends using this Fake Love Calculator.</h5>
                     <ul class="text-white">
-                       <li>Redister and share your link.</li> 
-                       <li>Check your Friend's Secret Crush on your Secret link.</li>
+                       <li type="none">Redister and share your link.</li> 
+                       <li type="none">Check your Friend's Secret Crush on your Secret link.</li>
                     </ul>
-                    <div><img src="{{ asset('assets/images/kopa.png') }}" width="200px" alt=""></div>
-                    <div class="pt-5 ml-5 mr-5 pb-5">
+                    <div><img src="{{ asset('assets/images/kopa.png') }}" width="140px" alt=""></div>
+                    <div class="pt-3 ml-5 mr-5 pb-2">
                         <form action="/love" method="post">
                             @csrf
                             <div class="pt-4">
                                 <label for="" class="text-white">Name</label>
                                 <input type="text"  name="name" class="form-control input" placeholder="Name">
                             </div>
-                            <div class="pt-5 pb-4">
+                            <div class="pt-2 pb-2">
                                 <button id="submit" class="btn btn-dark pt-2 pb-2" style="width: 100%"> <span class="fa fa-heart"></span> <strong>&nbsp;&nbsp; Register &nbsp;&nbsp;<span class="fa fa-heart"></span> </strong></button>
                             </div>
                         </form>
