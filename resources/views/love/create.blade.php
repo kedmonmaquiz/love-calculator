a5ca6d500c85f28166033c8b0410a6f7360ab999
kedmonjoseph <jkedmon95@gmail.com>
2020-10-02 17:58:15 +0300

    cdn

--- resources/views/love/create.blade.php
+++ resources/views/love/create.blade.php
@@ -6,7 +6,7 @@
     <meta name="_token" content="{{csrf_token()}}">
     <meta name="theme-color" content="#6c012d">
     <title>Love Calculator</title>
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="icon" href="{{asset('assets/images/kopa.png')}}">
     <script src="{{asset('assets/js/fontawesome.js')}}"></script>
     <style>
@@ -115,10 +115,10 @@
             </div>
     </div>
 
-    <script src="{{asset('assets/js/jquery.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-    <script>
-       
-    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous">
+            </script>
 </body>
 </html>
