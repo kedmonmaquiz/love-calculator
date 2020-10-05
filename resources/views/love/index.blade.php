837e52142a03b6450495f34509a10730725482c5
kedmonjoseph <jkedmon95@gmail.com>
2020-10-05 15:50:06 +0300

    submission error

--- resources/views/love/index.blade.php
+++ resources/views/love/index.blade.php
@@ -123,6 +123,27 @@
                 </div>
                </div>
             </div>
+
+
+            <!-- Modal -->
+        <div class="modal fade" id="requirements" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">heheheheh</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                body
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
 
     
@@ -140,7 +161,7 @@
             var id = $('#id').val();
 
             if(!name || !crush ){
-                //
+                $('#requirements').modal('show');
             }else{
                 $.ajaxSetup({
                       headers:{
