ee37a83eedb12d9f2cb90f35e37f8129f4ef7f0d
kedmonjoseph <jkedmon95@gmail.com>
2020-10-07 02:57:48 +0300

    Fixing AdController

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,32 @@ class AdController extends Controller
     public function storeAd(Request $request){
     	$ad_type = $request->type;
     	$body = $request->body;
+    	$ad_name = "";
     	
     	switch($ad_type){
     		case 'ad1':
     		  $ad_type = 'ad1';
+    		  $ad_name = 'First';
+    		  break;
+
+    		case 'ad2':
+    		   $ad_type = 'ad2';
+    		   $ad_name = 'Second';
+    		   break;
+
+    		case 'ad3':
+    		   $ad_type = 'ad3';
+    		   $ad_name = 'Third';
+    		   break;
+
+    		case 'ad4':
+    		   $ad_type = 'ad4';
+    		   $ad_name = 'Fourth';
+    		   break;
+
+    		default:
+    		    $ad_type = "";
+    		    $ad_name = 'Unknown';
     	}
 
         $ad = Ad::where('type',$ad_type)->get();
@@ -23,7 +45,7 @@ class AdController extends Controller
             'type'=>$ad_type,
             'body'=>$body
           ]);
-          $msg = 'First Ad created successfully';
+          $msg = $ad_name.' Ad created successfully';
     	}
 
     	if($ad->count() == 1){
@@ -32,9 +54,9 @@ class AdController extends Controller
     			$ad->update([
     			'body'=>$body
     		 ]);
-    		  $msg ='First Ad updated successfully';
+    		  $msg =$ad_name.' Ad updated successfully';
     		}else{
-    			$msg ='Nothing to Update,The Ad you are Trying to update looks the same as previous';
+    			$msg ='Nothing to Update,The Ad('.$ad_name.' Ad) you are Trying to update looks the same as previous';
     		}
     		
     	}
