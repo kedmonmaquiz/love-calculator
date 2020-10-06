0dfa50fc91b7e4afbc3c137ce399d85ea8efa31a
kedmonjoseph <jkedmon95@gmail.com>
2020-10-06 13:09:49 +0300

    confirming master

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,8 @@ Route::get('/waiting',function(){
 	return view('love.waiting');
 });
 
-// Route::post('/ad1}','App\Http\Controllers\AdController@storeAd1');
-Route::get('/admin',function(){
-	return 'Hi Maquiz';
+Route::get('/mimi',function(){
+	return 'hello';
 });
 
 Auth::routes();
