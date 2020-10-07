a4e17236225f81db26afb044fcbb9d38993e2457
kedmonjoseph <jkedmon95@gmail.com>
2020-10-07 16:00:50 +0300

    Cleaning the Login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,9 +47,11 @@
                 <a class="navbar-brand text-white" href="{{ url('/home') }}">
                     {{ 'Love Calculator Admin'}}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                @auth
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                @endauth
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
